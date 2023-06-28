fd577b96cfffd9f4adc3d2a92f64751991a9d001
Kecső Tamás <kecsot08@gmail.com>
2023-06-28 20:25:55 +0200

    use AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { RecoilRoot } from "recoil"
 import TestPage from "./pages/auth/TestPage"
+import { AuthProvider } from "./auth/AuthProvider"
 
 function App() {
   return (
     <>
       <RecoilRoot>
-        <TestPage />
+        <AuthProvider>
+          <TestPage />
+        </AuthProvider>
       </RecoilRoot>
     </>
   )
